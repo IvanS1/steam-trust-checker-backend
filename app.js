95c361b643a5d310092f18f5a1978bafb70e3f58
1v4n54n705 <gamesthefactory@gmail.com>
2026-01-07 09:39:49 +0100

    no message

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ async function getProfile() {
   
         if (!icon || !icon.complete || icon.naturalWidth === 0) return;
   
-        ctx.drawImage(icon, y.left + 240, yPos - 8, 16, 16);
+        ctx.drawImage(icon, y.left + 230, yPos - 8, 16, 16);
 
       });
   
